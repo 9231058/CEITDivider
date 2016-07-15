95662e643c01b6fdcaae217cebb95fd78458ef4e
Parham Alvani <parham.alvani@gmail.com>
2016-07-15 20:31:36 +0430

    Select subtractor was improved.

--- select_subtractor.vhd
+++ select_subtractor.vhd
@@ -4,19 +4,19 @@ use IEEE.std_logic_signed.all;
 use IEEE.numeric_std.all;
 
 entity select_subtractor is
-	port(
-		    a : in std_logic_vector(7 downto 0);
-		    b : in std_logic_vector(7 downto 0);
-		    res : out std_logic_vector(7 downto 0);
-		    bout : out std_logic
+	generic (N : natural := 8);
+	port(a : in std_logic_vector(N - 1 downto 0);
+		b : in std_logic_vector(N - 1 downto 0);
+		res : out std_logic_vector(N - 1 downto 0);
+		bout : out std_logic
         );
 end select_subtractor;
 
 architecture rtl of select_subtractor is
-	signal subtract : std_logic_vector (8 downto 0);
+	signal subtract : std_logic_vector (N downto 0);
 begin
 	subtract <= ('0' & a) - ('0' & b);
-    	res <= a when subtract(8) = '1' else
-	       subtract(7 downto 0) when subtract(8) = '0';
-	bout <= subtract(8);
+    	res <= a when subtract(N) = '1' else
+	       subtract(N - 1 downto 0) when subtract(N) = '0';
+	bout <= subtract(N);
 end rtl;
