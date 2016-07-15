f94d15874635a1df1de201b78bce19145d9bde5f
Parham Alvani <parham.alvani@gmail.com>
2016-07-15 23:12:34 +0430

    Divider module was completed.

--- divider.vhd
+++ divider.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity divider_eight is
+entity divider is
 	generic (N : natural := 8;
 		M : natural := 4);
 	port(a : in std_logic_vector(N - 1 downto 0);
@@ -9,9 +9,9 @@ entity divider_eight is
 		q : out std_logic_vector(N - M downto 0);
 		r : out std_logic_vector(N - M - 1 downto 0);
 		ovf : out std_logic);
-end divider_eight;
+end divider;
 
-architecture gate_level of divider_eight is
+architecture rtl of divider is
 	component select_subtractor is
 		generic (N : natural := 8);
 		port(a : in std_logic_vector(N - 1 downto 0);
@@ -20,15 +20,19 @@ architecture gate_level of divider_eight is
 			bout : out std_logic);
 	end component;
 	for all:select_subtractor use entity work.select_subtractor;
+	
+	type collection is array (N - M + 1 downto 0) of std_logic_vector(N - 1 downto 0);
 
 	signal bout : std_logic_vector(N - M downto 0);
-	signal res : std_logic_vector(N - 1 downto 0);
+	signal res : collection := (others => (others => '0'));
+	signal inp : collection := (others => (others => '0'));
 begin
-	res <= a;
+	res(N - M + 1) <= a;
 	sss: for I in N - 1 downto M - 1 generate
-		ss:select_subtractor port map(res , (I downto I - M + 1 => b, others => '0'), res, bout(I - M + 1));
+		inp(I - M + 1)(I downto I - M + 1) <= b;
+		ss:select_subtractor port map(res(I - M + 2) , inp(I - M + 1), res(I - M + 1), bout(I - M + 1));
 	end generate;
 
-	r <= res(N - M - 1 downto 0);
+	r <= res(0)(N - M - 1 downto 0);
 	q <= not bout;
-end gate_level;
+end rtl;
